f85bb07067dc3720f7d51aecd10613a2480a64f4
Jed Simson <jed.simson@gmail.com>
2022-05-01 18:36:29 +1200

    fix: Remove unnecessary configuration class.

--- RecipeManager.Host/HostDependencyModule.cs
+++ RecipeManager.Host/HostDependencyModule.cs
@@ -1,6 +1,4 @@
 ﻿using Autofac;
-using Microsoft.Extensions.Configuration;
-using RecipeManager.Core.Configuration;
 using RecipeManager.Core.Data.Abstract;
 using RecipeManager.WebApi.Infrastructure;
 
@@ -13,19 +11,6 @@ namespace RecipeManager.Host
             builder
                 .RegisterType<AppSettingsConnectionStringProvider>()
                 .As<IConnectionStringProvider>();
-
-            builder
-                .Register(c =>
-                {
-                    var configuration = c.Resolve<IConfiguration>();
-
-                    return new AuthenticationConfiguration()
-                    {
-                        Authority = configuration.GetValue<string>("Authentication:Authority"),
-                        Audience = configuration.GetValue<string>("Authentication:Audience")
-                    };
-                })
-                .SingleInstance();
 
             builder.RegisterModule<WebApiDependencyModule>();
         }
